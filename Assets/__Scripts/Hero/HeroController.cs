8a6e60d754d202171eae770907c73dbe530d80e0
fbennettiv <fredbennettiv@gmail.com>
2024-03-29 16:45:33 -0400

    Added initial Hero movement and fixing rotations

--- Assets/__Scripts/Hero/HeroController.cs
+++ Assets/__Scripts/Hero/HeroController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class HeroController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Inscribed")]
+    public float speed = 30;
+    private Rigidbody rigid;
+
+    //[Header("Dynamic")]
+
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+
+        /*rigid.rotation = Quaternion.Euler(0, 90, -90);
+        rigid.constraints = RigidbodyConstraints.FreezeRotationY;
+        rigid.constraints = RigidbodyConstraints.FreezeRotationZ;*/
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Pull in information from Input Class
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        // Change transform.position based on the axis
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        rigid.transform.position = pos;
+        rigid.transform.rotation = Quaternion.Euler(vAxis,90,-90);
+        //rigid.transform.SetPositionAndRotation(pos, Quaternion.Euler(0,pos.x,0));
     }
 }
